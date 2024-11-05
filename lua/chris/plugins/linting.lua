613227312fcb8e23758c58ecfc81489a63ffaba0
Christian <Christianjuresh@gmail.com>
2024-11-05 17:10:34 +0000

    wrong pyflake8 name

--- lua/chris/plugins/linting.lua
+++ lua/chris/plugins/linting.lua
@@ -10,7 +10,7 @@ return {
 			javascriptreact = { "eslint_d" },
 			typescriptreact = { "eslint_d" },
 			svelte = { "eslint_d" },
-			python = { "pyproject-flake8" },
+			python = { "pflake8" },
 		}
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
